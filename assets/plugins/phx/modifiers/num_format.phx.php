be77abd7a05726de362817f074349f1bf2f31074
MrSwed <webmaster@sdcollection.com>
2014-03-18 16:25:48 +0400

    num_format: format numeric value

---
+++ assets/plugins/phx/modifiers/num_format.phx.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    * description: format a numeric value
+    * usage: [+variable:num_format=`decimals|dec_point|thousands_sep`+]
+    * 
+*/
+list($decimals,$dec_point,$thousands_sep) = explode("|",$options,3);
+if (!$decimals) $decimals = 2;
+if (!$dec_point) $dec_point = ".";
+
+return number_format($output,$decimals,$dec_point,$thousands_sep);
+
+?>
